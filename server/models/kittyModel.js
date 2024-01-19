f63c1aa321fe509b609ee00ca9f33a11093d745f
mejrimo <mejrimo.dev@gmail.com>
2024-01-19 16:26:20 +0100

    Schema defined

--- server/models/kittyModel.js
+++ server/models/kittyModel.js
@@ -2,6 +2,39 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const kittySchema = new Schema({});
+const kittySchema = new Schema(
+	{
+		image: {
+			type: String,
+			default: 'https://freesvg.org/img/1682268254kawaiicat1.png',
+		},
+		name: {
+			type: String,
+			required: [true, "can't be blank"],
+			match: [/^[a-zA-Z\sÀ-ÿ']+$/, 'is invalid'],
+		},
+		gender: {
+			type: String,
+			enum: ['M', 'F'],
+		},
+		age: {
+			type: String,
+		},
+		isPeopleFriendly: {
+			type: Boolean,
+		},
+		isAnimalFriendly: {
+			type: Boolean,
+		},
+		isSpecialNeeds: {
+			type: Boolean,
+		},
+		description: {
+			type: String,
+			required: [true, "can't be blank"],
+		},
+	},
+	{ timestamps: true }
+);
 
 export default mongoose.model('Kitty', kittySchema);
