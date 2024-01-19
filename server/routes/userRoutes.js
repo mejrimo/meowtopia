33c7eb76a558ad4a7a52cb975833254ce1985056
mejrimo <mejrimo.dev@gmail.com>
2024-01-19 12:23:00 +0100

    routes updated

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import {
+	authUser,
+	getUserProfile,
+	logoutUser,
+	registerUser,
+	updateUserProfile,
+} from '../controllers/userController';
 
 const router = express.Router();
 
-router.post('/');
-router.post('/auth');
-router.post('/logout');
-router.get('/profile');
-router.patch('/profile');
+router.post('/register', registerUser);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.get('/profile', getUserProfile);
+router.patch('/profile', updateUserProfile);
 
 export default router;
