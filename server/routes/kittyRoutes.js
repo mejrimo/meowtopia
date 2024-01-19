e494519f829cc71f9aedd84c0155a6b71b57a4bd
mejrimo <mejrimo.dev@gmail.com>
2024-01-19 19:40:10 +0100

    all routes added

--- server/routes/kittyRoutes.js
+++ server/routes/kittyRoutes.js
@@ -1,8 +1,19 @@
 import express from 'express';
-import { addKitty } from '../controllers/kittyController.js';
+import idChecker from '../middleware/idChecker.js';
+import {
+	addKitty,
+	deleteKitty,
+	getAllKitties,
+	getKitty,
+	updateKitty,
+} from '../controllers/kittyController.js';
 
 const router = express.Router();
 
 router.post('/addKitty', addKitty);
+router.get('/', getAllKitties);
+router.get('/:id', idChecker, getKitty);
+router.patch('/:id', idChecker, updateKitty);
+router.delete('/:id', idChecker, deleteKitty);
 
 export default router;
