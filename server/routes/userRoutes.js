0a64fabc3842338b817435944eded4251c001654
mejrimo <mejrimo.dev@gmail.com>
2024-01-20 11:08:33 +0100

    routes to favorites added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import { protect } from '../middleware/authMiddleware.js';
+import idChecker from '../middleware/idChecker.js';
 import {
 	authUser,
+	changeFavorites,
 	deleteUser,
+	getAllFavorites,
 	getUserProfile,
 	logoutUser,
 	registerUser,
 	updateUserProfile,
 } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
@@ -17,5 +20,12 @@ router.post('/logout', logoutUser);
 router.get('/profile', protect, getUserProfile);
 router.patch('/profile', protect, updateUserProfile);
 router.delete('/profile', protect, deleteUser);
+router.delete('/profile/favorites', protect, getAllFavorites);
+router.delete(
+	'/profile/favorites/:kittyId',
+	protect,
+	idChecker,
+	changeFavorites
+);
 
 export default router;
