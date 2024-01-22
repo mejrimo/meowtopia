97fcbdefd83a1c8ac4f049d8e6dad019ca50ee9e
mejrimo <mejrimo.dev@gmail.com>
2024-01-22 18:42:16 +0100

    base components and routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,22 @@
-const App = () => {
-	return <div>App</div>;
-};
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Layout />}>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/login" element={<Login />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
 export default App;
