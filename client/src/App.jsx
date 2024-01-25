f6f356e5f9179b7ac557a8d29e5583a9857901a7
mejrimo <mejrimo.dev@gmail.com>
2024-01-25 14:31:30 +0100

    routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
+import Registration from './pages/Registration';
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Registration />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
