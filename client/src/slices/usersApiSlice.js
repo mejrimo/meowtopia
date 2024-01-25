9769a026fb090304c4a64251b4a9e70889837608
mejrimo <mejrimo.dev@gmail.com>
2024-01-25 14:30:57 +0100

    slices completed

---
+++ client/src/slices/usersApiSlice.js
@@ -0,0 +1,61 @@
+import { apiSlice } from './apiSlice';
+const USERS_URL = '/api/users';
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		login: builder.mutation({
+			query: (data) => ({
+				url: `${USERS_URL}/auth`,
+				method: 'POST',
+				body: data,
+			}),
+
+			register: builder.mutation({
+				query: (data) => ({
+					url: `${USERS_URL}/register`,
+					method: 'POST',
+					body: data,
+				}),
+			}),
+
+			logout: builder.mutation({
+				query: () => ({
+					url: `${USERS_URL}/logout`,
+					method: 'POST',
+				}),
+			}),
+
+			updateUser: builder.mutation({
+				query: (data) => ({
+					url: `${USERS_URL}/profile`,
+					method: 'PATCH',
+					body: data,
+				}),
+			}),
+
+			deleteUser: builder.mutation({
+				query: () => ({
+					url: `${USERS_URL}/profile`,
+					method: 'DELETE',
+				}),
+			}),
+
+			updateFavorites: builder.mutation({
+				query: (data) => ({
+					url: `${USERS_URL}/profile/favorites/:kittyId`,
+					method: 'POST',
+					body: data,
+				}),
+			}),
+		}),
+	}),
+});
+
+export const {
+	useLoginMutation,
+	useRegisterMutation,
+	useLogoutMutation,
+	useUpdateUserMutation,
+	useDeleteUserMutation,
+	useUpdateFavoritesMutation,
+} = usersApiSlice;
