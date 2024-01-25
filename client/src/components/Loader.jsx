cd32c3729965987bc440c5042c2654a9cbc9526c
mejrimo <mejrimo.dev@gmail.com>
2024-01-25 14:33:15 +0100

    spinner loader added

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,16 @@
+import { PuffLoader } from 'react-spinners';
+
+const Loader = () => {
+	return (
+		<div className="block m-auto">
+			<PuffLoader
+				height="80"
+				width="80"
+				radius={1}
+				color="#4c1d95"
+				aria-label="puff-loading"
+			/>
+		</div>
+	);
+};
+export default Loader;
