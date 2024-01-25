0bd480210e6f8afb4de82912f64d05ddecf7b339
mejrimo <mejrimo.dev@gmail.com>
2024-01-25 14:31:49 +0100

    store added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import store from './store.js';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import App from './App.jsx';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<Provider store={store}>
+		<React.StrictMode>
+			<App />
+			<ToastContainer />
+		</React.StrictMode>
+	</Provider>
 );
