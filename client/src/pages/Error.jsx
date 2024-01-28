e7bc05e58e8c87a1a7f5c309e7390fce6429e876
mejrimo <mejrimo.dev@gmail.com>
2024-01-28 18:00:09 +0100

    error page created

---
+++ client/src/pages/Error.jsx
@@ -0,0 +1,10 @@
+const Error = () => {
+	return (
+		<div className="flex items-center justify-center h-screen text-center">
+			<span className="text-2xl text-bold text-zinc-700 md:text-3xl">
+				Sorry! There was an error. We'll be fixing it soon...
+			</span>
+		</div>
+	);
+};
+export default Error;
