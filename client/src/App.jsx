998611a0ecaa7d175bfbd9edc1a4cc4633be9068
mejrimo <mejrimo.dev@gmail.com>
2024-01-28 17:55:24 +0100

    routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
+import Error from './pages/Error';
+import Kitty from './pages/Kitty';
+import PrivateRoute from './components/PrivateRoute';
+import Profile from './pages/Profile';
+import Contact from './pages/Contact';
 
 function App() {
 	return (
@@ -15,6 +20,12 @@ function App() {
 					<Route path="/about" element={<About />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/register" element={<Registration />} />
+					<Route path="/error" element={<Error />} />
+					<Route path="/kitty/:id" element={<Kitty />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="" element={<PrivateRoute />}>
+						<Route path="/profile" element={<Profile />} />
+					</Route>
 				</Route>
 			</Routes>
 		</BrowserRouter>
