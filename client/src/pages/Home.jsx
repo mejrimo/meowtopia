702a69dea6c3f979e4df892e45b0827da590f737
mejrimo <mejrimo.dev@gmail.com>
2024-01-28 18:02:00 +0100

    home page component cleaned

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,6 @@
 import Kitties from '../components/Kitties';
 
 const Home = () => {
-	return (
-		<div className="container mx-auto ">
-			<div className="grid grid-cols-2 gap-5 mx-auto my-10 md:grid-cols-3 lg:grid-cols-4 bg-zinc-100">
-				<Kitties />
-			</div>
-		</div>
-	);
+	return <Kitties />;
 };
 export default Home;
