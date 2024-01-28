495f42c8b48b4d184fc121d32a72a973f069ed38
mejrimo <mejrimo.dev@gmail.com>
2024-01-28 18:00:50 +0100

    all kitties component added

--- client/src/components/Kitties.jsx
+++ client/src/components/Kitties.jsx
@@ -1,4 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+
+import Loader from './Loader';
+import { useKitties } from '../hooks/useKitties';
+import KittyCard from './KittyCard';
+
 const Kitties = () => {
-	return <div>Kitties</div>;
+	const { isLoading, isError, data } = useKitties();
+
+	const navigate = useNavigate();
+
+	if (isError) {
+		navigate('/error');
+	}
+
+	if (isLoading) {
+		return <Loader />;
+	}
+
+	return (
+		<div className="container mx-auto">
+			<div className="grid grid-cols-2 my-10 md:grid-cols-3 lg:grid-cols-4 bg-zinc-100 justify-items-center">
+				<KittyCard data={data} />
+			</div>
+		</div>
+	);
 };
 export default Kitties;
