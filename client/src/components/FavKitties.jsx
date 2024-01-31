f3ce9e9c4b1b4c76c38dc3ee2a3331dae1e9ebf2
mejrimo <mejrimo.dev@gmail.com>
2024-01-31 17:07:28 +0100

    component for favorites kitties added

--- client/src/components/FavKitties.jsx
+++ client/src/components/FavKitties.jsx
@@ -1,4 +1,35 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import Loader from './Loader';
+import { useFetchKitties } from '../hooks/useFetchKitties';
+import KittyCard from './KittyCard';
+import { useGetFavKittiesIdQuery } from '../slices/usersApiSlice';
+
 const FavKitties = () => {
-	return <div>FavKitties</div>;
+	const { isLoading, isError, kittiesData } = useFetchKitties('kitties');
+	const { data } = useGetFavKittiesIdQuery();
+
+	const navigate = useNavigate();
+
+	if (isError) {
+		navigate('/error');
+	}
+
+	if (isLoading) {
+		return <Loader />;
+	}
+
+	const favoritesElements = kittiesData.filter((kitty) =>
+		data?.includes(kitty._id)
+	);
+
+	return (
+		<div className="container mx-auto">
+			<div className="grid grid-cols-2 my-10 md:grid-cols-3 lg:grid-cols-4 bg-zinc-100 justify-items-center">
+				<KittyCard data={favoritesElements} />
+			</div>
+		</div>
+	);
 };
 export default FavKitties;
