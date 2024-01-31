a86b503a63246bf3ab2a2dee55b5b8556117b99a
mejrimo <mejrimo.dev@gmail.com>
2024-01-31 21:58:08 +0100

    cors

--- server/server.js
+++ server/server.js
@@ -31,11 +31,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //cors
 app.use(cors());
-app.use(
-	cors({
-		origin: 'https://meowtopia-kappa.vercel.app/',
-	})
-);
 
 //DEBUG middleware
 app.use((req, res, next) => {
