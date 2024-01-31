df8a5da3b9c7538f9b2504e5a450dfcad5f26121
mejrimo <mejrimo.dev@gmail.com>
2024-01-31 23:59:44 +0100

    token

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON requests
 app.use(express.json());
 //cors
-app.use(cors());
+app.use(cors({ credentials: true }));
 
 //DEBUG middleware
 app.use((req, res, next) => {
