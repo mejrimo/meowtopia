154df825be3cfd2b34b9de97f3c0e9c01cab3b31
mejrimo <mejrimo.dev@gmail.com>
2024-01-31 23:11:41 +0100

    token

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,7 +5,6 @@ import User from '../models/userModel.js';
 const protect = asyncHandler(async (req, res, next) => {
 	let token;
 	token = req.cookies.jwt;
-	console.log(req.cookies);
 
 	if (token) {
 		try {
