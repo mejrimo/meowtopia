862e95b12019ed857984520e57f45b05ac3e4191
mejrimo <mejrimo.dev@gmail.com>
2024-02-01 14:11:10 +0100

    deploy

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: 'https://meowtopia-server.onrender.com/api',
+	baseUrl: 'https://meowtopia-server.onrender.com',
 }); // with no proxy -> baseUrl:'http://localhost:8000'
 
 export const apiSlice = createApi({
