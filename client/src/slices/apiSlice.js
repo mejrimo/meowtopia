420649ec6cd94f5711ffd20c83e79aae6a0d5162
mejrimo <mejrimo.dev@gmail.com>
2024-02-01 14:23:11 +0100

    deploy

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: 'https://meowtopia-server.onrender.com',
-	changeOrigin: true,
+	baseUrl: '',
 }); // with no proxy -> baseUrl:'http://localhost:8000'
 
 export const apiSlice = createApi({
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	// server: {
-	// 	proxy: {
-	// 		'/api': {
-	// 			target: 'http://localhost:8000',
-	// 			changeOrigin: true,
-	// 		},
-	// 	},
-	// },
+	server: {
+		proxy: {
+			'/api': {
+				target: 'https://meowtopia-server.onrender.com',
+				changeOrigin: true,
+			},
+		},
+	},
 });
