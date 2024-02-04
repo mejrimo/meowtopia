aacfe92deda0019d4dae5619b4cc0c662b68ff45
mejrimo <mejrimo.dev@gmail.com>
2024-02-04 08:17:29 +0100

    cookie

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -6,10 +6,10 @@ const generateToken = (res, userId) => {
 	});
 
 	res.cookie('jwt', token, {
-		httpOnly: true,
+		httpOnly: false,
 		domain: 'onrender.com',
 		secure: process.env.NODE_ENV !== 'development',
-		sameSite: 'strict',
+		sameSite: 'none',
 		maxAge: 30 * 24 * 60 * 60 * 1000,
 	});
 };
