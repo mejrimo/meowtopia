459a0aea1914f18bc6b72bc9b3648fda09aec721
mejrimo <mejrimo.dev@gmail.com>
2024-02-08 04:39:07 +0100

    custom onClick function added

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,6 +1,7 @@
-const Button = ({ children, className }) => {
+const Button = ({ children, className, onClick }) => {
 	return (
 		<button
+			onClick={onClick}
 			className={`px-6 py-2 font-medium rounded-md shadow bg-violet-700 text-zinc-100 hover:bg-violet-900 active:bg-violet-500 + ${className}`}
 		>
 			{children}
