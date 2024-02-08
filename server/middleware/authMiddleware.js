44e0e1984be72416526d9a9b8feee0e9e4b0b5fd
mejrimo <mejrimo.dev@gmail.com>
2024-02-08 04:34:06 +0100

    token validation changed

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -3,23 +3,24 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 
 const protect = asyncHandler(async (req, res, next) => {
-	let token;
-	token = req.cookies.jwt;
+	const { authorization } = req.headers;
 
-	if (token) {
-		try {
-			const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-			req.user = await User.findById(decoded.userId).select('-password');
-
-			next();
-		} catch (err) {
-			res.status(401);
-			throw new Error('Not authorized, invalid token');
-		}
-	} else {
+	if (!authorization) {
 		res.status(401);
 		throw new Error('Not authorized, no token');
+	}
+
+	const token = authorization.split(' ')[1];
+
+	try {
+		const { _id } = jwt.verify(token, process.env.JWT_SECRET);
+
+		req.user = await User.findById(_id).select('_id');
+		req.token = token;
+		next();
+	} catch (err) {
+		res.status(401);
+		throw new Error('Not authorized, invalid token');
 	}
 });
 
