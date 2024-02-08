e1bbbd49493dfd00fc011f5f7202579189257301
mejrimo <mejrimo.dev@gmail.com>
2024-02-08 04:34:32 +0100

    useless routes deleted

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,9 +4,7 @@ import {
 	authUser,
 	changeFavorites,
 	deleteUser,
-	getAllFavorites,
 	getUserProfile,
-	logoutUser,
 	registerUser,
 	updateUserProfile,
 } from '../controllers/userController.js';
@@ -15,11 +13,9 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/auth', authUser);
-router.post('/logout', logoutUser);
 router.get('/profile', protect, getUserProfile);
 router.patch('/profile', protect, updateUserProfile);
 router.delete('/profile', protect, deleteUser);
-router.get('/profile/favorites', protect, getAllFavorites);
 router.post('/profile/favorites/:kittyId', protect, changeFavorites);
 
 export default router;
