6f347487172845af34f48606eb6d3f5402a4086a
mejrimo <mejrimo.dev@gmail.com>
2024-02-08 04:31:39 +0100

    prepareHeaders added to use authorization header

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -2,6 +2,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
 	baseUrl: '',
+	prepareHeaders: (headers) => {
+		const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+		if (userInfo) {
+			const { token } = userInfo;
+
+			if (token) {
+				headers.set('Authorization', `Bearer ${token}`);
+			}
+		}
+
+		return headers;
+	},
 }); // with no proxy -> baseUrl:'http://localhost:8000'
 
 export const apiSlice = createApi({
