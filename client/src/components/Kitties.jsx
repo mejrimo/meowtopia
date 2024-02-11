a5f16c71b9b38ca7797bc59e461090f61d0dee05
mejrimo <mejrimo.dev@gmail.com>
2024-02-11 06:24:22 +0100

    render logic changed

--- client/src/components/Kitties.jsx
+++ client/src/components/Kitties.jsx
@@ -9,20 +9,16 @@ const Kitties = () => {
 
 	const navigate = useNavigate();
 
-	if (isError) {
-		navigate('/error');
-	}
-
-	if (isLoading) {
-		return <Loader />;
-	}
-
 	return (
-		<div className="container mx-auto">
-			<div className="grid grid-cols-2 my-10 md:grid-cols-3 lg:grid-cols-4 bg-zinc-100 justify-items-center">
+		<>
+			{isError ? (
+				navigate('/error')
+			) : isLoading ? (
+				<Loader />
+			) : (
 				<KittyCard data={kittiesData} />
-			</div>
-		</div>
+			)}
+		</>
 	);
 };
 export default Kitties;
