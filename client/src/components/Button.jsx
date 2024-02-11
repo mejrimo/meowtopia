dff38943ecd9c05c4c077a4acccaf965c0ef4ec7
mejrimo <mejrimo.dev@gmail.com>
2024-02-11 04:16:21 +0100

    style changed

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,7 +1,8 @@
-const Button = ({ children, className }) => {
+const Button = ({ children, className, clickFunc }) => {
 	return (
 		<button
-			className={`px-6 py-2 font-medium rounded-md shadow bg-violet-700 text-zinc-100 hover:bg-violet-900 active:bg-violet-500 + ${className}`}
+			onClick={clickFunc}
+			className={`cursor-pointer px-6 py-2 font-medium rounded-md shadow bg-violet-700 text-zinc-100 hover:bg-violet-900 active:bg-violet-500 + ${className}`}
 		>
 			{children}
 		</button>
--- client/src/components/SubmitButton.jsx
+++ client/src/components/SubmitButton.jsx
@@ -2,7 +2,7 @@ const SubmitButton = ({ children }) => {
 	return (
 		<button
 			type="submit"
-			className="px-6 py-2 mx-1 my-3 font-medium rounded-md shadow bg-violet-700 text-zinc-100 hover:bg-violet-900 active:bg-violet-500"
+			className="px-6 py-2 mx-1 my-3 font-medium rounded-md shadow cursor-pointer bg-violet-700 text-zinc-100 hover:bg-violet-900 active:bg-violet-500"
 		>
 			{children}
 		</button>
