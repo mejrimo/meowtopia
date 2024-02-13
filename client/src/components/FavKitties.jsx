3016ee9b9c152a8481e24c95d1d157b39e158733
mejrimo <mejrimo.dev@gmail.com>
2024-02-13 01:05:02 +0100

    style changed

--- client/src/components/FavKitties.jsx
+++ client/src/components/FavKitties.jsx
@@ -11,24 +11,20 @@ const FavKitties = () => {
 
 	const navigate = useNavigate();
 
-	if (isError) {
-		navigate('/error');
-	}
-
-	if (isLoading) {
-		return <Loader />;
-	}
-
 	const favoritesElements = kittiesData.filter((kitty) =>
 		userInfo?.favKittiesId?.includes(kitty._id)
 	);
 
 	return (
-		<div className="container mx-auto">
-			<div className="grid grid-cols-2 my-10 md:grid-cols-3 lg:grid-cols-4 bg-zinc-100 justify-items-center">
+		<>
+			{isError ? (
+				navigate('/error')
+			) : isLoading ? (
+				<Loader />
+			) : (
 				<KittyCard data={favoritesElements} />
-			</div>
-		</div>
+			)}
+		</>
 	);
 };
 export default FavKitties;
